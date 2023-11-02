3619bdeebdc6e6f47ad2771aac4b7842aaf8ce33
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2023-11-02 17:50:41 +0900

    update

---
+++ src/pages/api/crawling.ts
@@ -0,0 +1,72 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+import * as cheerio from "cheerio";
+import type { NextApiRequest, NextApiResponse } from "next";
+import iconv from "iconv-lite";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const keyword: any = req.query.keyword;
+
+  const DOMAIN = "https://finance.naver.com";
+
+  // HTML 크롤링 해오는 함수
+  async function crawlingHTML(url: string) {
+    const { data: html } = await axios({
+      url: url,
+      method: "GET",
+      responseType: "arraybuffer",
+    });
+    const content = await iconv.decode(html, "EUC-KR").toString();
+    return content;
+  }
+
+  // 회사 코드
+  const companyCode = [] as any[];
+  for (let i = 0; i < 43; i++) {
+    const companySelector = cheerio.load(
+      await crawlingHTML(`${DOMAIN}/sise/sise_market_sum.naver?&page=${i + 1}`)
+    );
+    companySelector("tbody tr .center a").each((_, item) => {
+      companyCode.push(companySelector(item).attr("href")?.slice(-6));
+    });
+  }
+
+  console.log(companyCode);
+
+  //결과 값
+  const result = [] as object[];
+  companyCode.forEach((code) => {});
+  for (let i = 0; i < companyCode.length; i++) {
+    // DOM 조작을 위한 selector 설정
+    const selector = cheerio.load(
+      await crawlingHTML(`${DOMAIN}/item/news.naver?code=${companyCode[i]}`)
+    );
+    const iframeURL = `${DOMAIN}${selector("#news_frame").attr("src")}`;
+    const selectorPostList = cheerio.load(await crawlingHTML(iframeURL));
+
+    // 회사명
+    const company = selector(".wrap_company a").text();
+
+    // 제목
+    selectorPostList(".tb_cont tbody tr").each((_, item) => {
+      if (selectorPostList(item).text().trim().includes(keyword)) {
+        result.push({
+          company,
+          title: selectorPostList(item).find(".title a").text().trim(),
+          href: DOMAIN + selectorPostList(item).find(".title a").attr("href"),
+          date: selectorPostList(item).find(".date").text().split(" ")[1],
+        });
+      }
+    });
+  }
+
+  // 회사 수 설정 할 수 있게 => page 수
+  // 코스닥 코스피 선택 할 수 있게
+  return res
+    .setHeader("Access-Control-Allow-Origin", "*")
+    .status(200)
+    .json(result);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import axios from "axios";
+import Head from "next/head";
+import Link from "next/link";
+import { ChangeEvent, FormEvent, useState } from "react";
+import Parser from "html-react-parser";
 
-const inter = Inter({ subsets: ['latin'] })
+interface IData {
+  company: string;
+  title: string;
+  href: string;
+  date: string;
+}
 
 export default function Home() {
+  const [value, setValue] = useState("");
+  const [keyWord, setKeyWord] = useState("");
+  const [data, setData] = useState<IData[]>();
+  const [loading, setLoading] = useState(false);
+
+  const onClick = async () => {
+    setKeyWord(value);
+    setLoading(true);
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_HOST}/api/crawling?keyword=${value}`
+    );
+    setLoading(false);
+    setData(data);
+    setValue("");
+  };
+
   return (
     <>
       <Head>
@@ -14,101 +36,44 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+      <div className="container">
+        <div>
+          <form onSubmit={(e: FormEvent) => e.preventDefault()}>
+            <input
+              value={value}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setValue(e.target.value)
+              }
+            />
+            <button onClick={onClick}>검색</button>
+          </form>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+        <div>검색 키워드 : {keyWord}</div>
+        <div>
+          <ul>
+            {loading ? (
+              <div>로딩 중..</div>
+            ) : (
+              data?.map(({ company, title, href, date }, idx) => (
+                <li key={idx} className="item_list">
+                  <div className="item_company">{company}</div>
+                  <Link target="_blank" href={href}>
+                    <div>
+                      {Parser(
+                        title.replaceAll(
+                          `${keyWord}`,
+                          `<mark style='background-color: #b2ddfc; font-weight:bold'>${keyWord}</mark>`
+                        )
+                      )}
+                    </div>
+                  </Link>
+                  <div>{date}</div>
+                </li>
+              ))
+            )}
+          </ul>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      </div>
     </>
-  )
+  );
 }
